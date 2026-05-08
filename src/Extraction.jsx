42956c515b1a4f365420516a100c151b4c30a22e
Deekshith-dpod <deekshith@dpod.io>
2026-05-08 12:59:07 +0530

    local-qat config

--- src/Extraction.jsx
+++ src/Extraction.jsx
@@ -8,11 +8,22 @@ function ExtractionLauncher() {
 
     useEffect(() => {
 
+        // local-dev
+        // const iframeconfig = {
+        //     appflyte_backend_url: "https://api-dev.appflyte.net",
+        //     appflyte_agent_api_token: "a5febdcf-d165-4cac-81de-b4983570caed",
+        //     appflyte_project_id: "a167154f-df7d-43f1-88c7-f1657a826a68",
+        //     extraction_task_id: "98ee16f1-22e4-4fd5-a8a3-bcec867dd20c",
+        //     onCancel: () => console.log("hi"),
+        //     onSave: () => console.log("hi")
+        // };
+
+        // local-qat
         const iframeconfig = {
-            appflyte_backend_url: "https://api-dev.appflyte.net",
-            appflyte_agent_api_token: "a5febdcf-d165-4cac-81de-b4983570caed",
-            appflyte_project_id: "a167154f-df7d-43f1-88c7-f1657a826a68",
-            extraction_task_id: "98ee16f1-22e4-4fd5-a8a3-bcec867dd20c",
+            appflyte_backend_url: "https://backend.sandbox.ameya.ai",
+            appflyte_agent_api_token: "184451e7-bb82-4ce1-b833-4d4032768ee9",
+            appflyte_project_id: "ae7e5874-edd1-4cbd-8ee6-b85119a29abf",
+            extraction_task_id: "0e90710f-2732-4df6-8236-3e10ae1d0402",
             onCancel: () => console.log("hi"),
             onSave: () => console.log("hi")
         };
@@ -49,7 +60,8 @@ function ExtractionLauncher() {
 
         const script = iframeDoc.createElement("script");
         // script.src = 'https://dpod-aws-s3.s3.us-east-1.amazonaws.com/web-plugins/ameya-extraction/sfs/test/ameya-extraction.js';
-        script.src = 'https://ameya-extraction.ameya.ai';
+        // script.src = 'https://ameya-extraction.ameya.ai';
+        script.src = 'https://ameya-extraction.ameya.ai'
 
         script.onload = () => {
             iframeconfig.onCancel = () => {
